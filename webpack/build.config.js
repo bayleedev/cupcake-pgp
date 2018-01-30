3a18b26505482a39c462ec1b3c8861df2e047d3f
Blaine Schmeisser <blainesch@gmail.com>
2018-01-30 12:50:45 -0800

    Move webpack to it's own directory.

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BabiliPlugin = require('babili-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const { spawn } = require('child_process')
 
 // Config directories
-const SRC_DIR = path.resolve(__dirname, 'src')
-const OUTPUT_DIR = path.resolve(__dirname, 'dist')
+const SRC_DIR = path.resolve(__dirname, '..', 'src')
+const OUTPUT_DIR = path.resolve(__dirname, '..', 'dist')
 
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
 const defaultInclude = [SRC_DIR]
@@ -15,18 +16,17 @@ module.exports = {
   entry: SRC_DIR + '/index.js',
   output: {
     path: OUTPUT_DIR,
-    publicPath: '/',
+    publicPath: './',
     filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }),
         include: defaultInclude
       },
       {
@@ -54,26 +54,16 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: 'src/pgp-sw.js' },
     ]),
+    new ExtractTextPlugin('bundle.css'),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new BabiliPlugin()
   ],
-  devtool: 'cheap-source-map',
-  devServer: {
-    contentBase: OUTPUT_DIR,
-    stats: {
-      colors: true,
-      chunks: false,
-      children: false
-    },
-    setup() {
-      spawn(
-        'electron',
-        ['.'],
-        { shell: true, env: process.env, stdio: 'inherit' }
-      )
-      .on('close', code => process.exit(0))
-      .on('error', spawnError => console.error(spawnError))
-    }
+  stats: {
+    colors: true,
+    children: false,
+    chunks: false,
+    modules: false
   }
 }
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,13 +1,12 @@
 const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BabiliPlugin = require('babili-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { spawn } = require('child_process')
 
 // Config directories
-const SRC_DIR = path.resolve(__dirname, 'src')
-const OUTPUT_DIR = path.resolve(__dirname, 'dist')
+const SRC_DIR = path.resolve(__dirname, '..', 'src')
+const OUTPUT_DIR = path.resolve(__dirname, '..', 'dist')
 
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
 const defaultInclude = [SRC_DIR]
@@ -16,17 +15,18 @@ module.exports = {
   entry: SRC_DIR + '/index.js',
   output: {
     path: OUTPUT_DIR,
-    publicPath: './',
+    publicPath: '/',
     filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        }),
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
         include: defaultInclude
       },
       {
@@ -54,16 +54,26 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: 'src/pgp-sw.js' },
     ]),
-    new ExtractTextPlugin('bundle.css'),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new BabiliPlugin()
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
   ],
-  stats: {
-    colors: true,
-    children: false,
-    chunks: false,
-    modules: false
+  devtool: 'cheap-source-map',
+  devServer: {
+    contentBase: OUTPUT_DIR,
+    stats: {
+      colors: true,
+      chunks: false,
+      children: false
+    },
+    setup() {
+      spawn(
+        'electron',
+        ['.'],
+        { shell: true, env: process.env, stdio: 'inherit' }
+      )
+      .on('close', code => process.exit(0))
+      .on('error', spawnError => console.error(spawnError))
+    }
   }
 }
